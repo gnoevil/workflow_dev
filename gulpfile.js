cfcac1d6cb5cb2de27a99b016ceaaa4bd544e3d5
gnoevil <geno.guerrero@gmail.com>
2016-09-22 13:19:07 -0700

    added watch task to default

--- gulpfile.js
+++ gulpfile.js
@@ -48,4 +48,4 @@ gulp.task('watch', function() {
   gulp.watch('builds/development/images/**/*.*', ['images']);
 });
 
-gulp.task('default',  ['coffee', 'js', 'compass']);
+gulp.task('default',  ['coffee', 'js', 'compass', 'watch']);
